16c01625799bc126232ed2914df79f9b183fca8f
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-09-17 18:00:21 -0400

    Start of Fabric port

---
+++ src/main/java/mod/azure/jarjarbinks/JarJarBinksMod.java
@@ -0,0 +1,10 @@
+package mod.azure.jarjarbinks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class JarJarBinksMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
